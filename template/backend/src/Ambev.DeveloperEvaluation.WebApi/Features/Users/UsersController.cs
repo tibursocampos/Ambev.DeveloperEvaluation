8e929172b638a3e0bbecadce2dc186b5feee4c2e
Raphael Campos <raphael_pcampos@outlook.com>
2025-01-29 18:31:51 -0300

    refact: adjustments to references after changing the Common folder location

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Users/UsersController.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Users/UsersController.cs
@@ -8,6 +8,7 @@ using Ambev.DeveloperEvaluation.WebApi.Features.Users.DeleteUser;
 using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
 using Ambev.DeveloperEvaluation.Application.Users.GetUser;
 using Ambev.DeveloperEvaluation.Application.Users.DeleteUser;
+using Ambev.DeveloperEvaluation.Application.Common;
 
 namespace Ambev.DeveloperEvaluation.WebApi.Features.Users;
 
