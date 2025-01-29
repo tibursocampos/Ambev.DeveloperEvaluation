4d33286a939336264d57ebffc0035f0b29e6314d
Raphael Campos <raphael_pcampos@outlook.com>
2025-01-29 20:16:06 -0300

    feat: configuring RabbitMQ for messaging

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Common/RabbitMQ/IRabbitMQService.cs
@@ -0,0 +1,14 @@
+﻿namespace Ambev.DeveloperEvaluation.Common.RabbitMQ;
+
+/// <summary>
+/// Interface for RabbitMQ service to send messages to a queue.
+/// </summary>
+public interface IRabbitMQService
+{
+    /// <summary>
+    /// Sends a message to the RabbitMQ queue.
+    /// </summary>
+    /// <typeparam name="T">The type of the message to send.</typeparam>
+    /// <param name="message">The message to send.</param>
+    void SendMessage<T>(T message);
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Common/RabbitMQ/RabbitMQSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Ambev.DeveloperEvaluation.Common.RabbitMQ;
+
+public class RabbitMQSettings
+{
+    public string HostName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string QueueName { get; set; }
+}
