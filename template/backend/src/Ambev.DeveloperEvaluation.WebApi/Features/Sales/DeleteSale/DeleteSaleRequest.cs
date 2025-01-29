031a7ccd85dd15ea6c9ec9b86945fab0b50f2236
Raphael Campos <raphael_pcampos@outlook.com>
2025-01-29 18:35:18 -0300

    feat: delete sale webapi

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/DeleteSale/DeleteSaleRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.DeleteSale;
+
+/// <summary>
+/// API request model for DeleteSale operation
+/// </summary>
+public class DeleteSaleRequest
+{
+    /// <summary>
+    /// The unique identifier of the sale to delete
+    /// </summary>
+    public Guid Id { get; set; }
+}
