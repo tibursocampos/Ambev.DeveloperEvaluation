c895edb723b9af0f22a006edfc17d8a8ce0215a2
Raphael Campos <raphael_pcampos@outlook.com>
2025-01-29 18:35:32 -0300

    feat: get sale by id webapi

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetSale/GetSaleProfile.cs
@@ -0,0 +1,23 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale;
+
+/// <summary>
+/// Profile for mapping GetSale feature requests to queries and results to responses
+/// </summary>
+public class GetSaleProfile : Profile
+{
+    /// <summary>
+    /// Initializes the mappings for GetSale feature
+    /// </summary>
+    public GetSaleProfile()
+    {
+        CreateMap<Guid, GetSaleQuery>()
+            .ConstructUsing(id => new GetSaleQuery(id));
+
+        CreateMap<GetSaleResult, GetSaleResponse>();
+        CreateMap<GetSaleItemResult, GetSaleItemResponse>();
+    }
+}
