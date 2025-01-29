8ebe137b49cb30615e7b30e65c89bb98133f1d3b
Raphael Campos <raphael_pcampos@outlook.com>
2025-01-29 06:39:46 -0300

    feat: creating Strategy to apply discounts to purchases

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Strategies/Discount/NoDiscountStrategy.cs
@@ -0,0 +1,9 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Strategies.Discount;
+
+public class NoDiscountStrategy : IDiscountStrategy
+{
+    public decimal CalculateDiscount(int quantity, decimal unitPrice)
+    {
+        return 0;
+    }
+}
