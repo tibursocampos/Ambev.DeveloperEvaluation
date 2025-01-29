8a73cb97f150fbce02404b38ef714ced082a45b7
Raphael Campos <raphael_pcampos@outlook.com>
2025-01-29 18:37:14 -0300

    feat: get sale by id application (query and handler)

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleQuery.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+
+/// <summary>
+/// Query for retrieving a sale by its unique identifier
+/// </summary>
+public class GetSaleQuery : IRequest<GetSaleResult>
+{
+    /// <summary>
+    /// The unique identifier of the sale
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the GetSaleQuery
+    /// </summary>
+    /// <param name="id">The unique identifier of the sale</param>
+    public GetSaleQuery(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/GetSale/GetSaleValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+
+/// <summary>
+/// Validator for GetSaleQuery
+/// </summary>
+public class GetSaleValidator : AbstractValidator<GetSaleQuery>
+{
+    /// <summary>
+    /// Initializes validation rules for GetSaleQuery
+    /// </summary>
+    public GetSaleValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Sale ID is required");
+    }
+}
