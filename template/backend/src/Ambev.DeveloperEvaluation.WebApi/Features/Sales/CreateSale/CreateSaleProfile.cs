2a575e4009b98d6d100dd20a529cf0e183e9f229
Raphael Campos <raphael_pcampos@outlook.com>
2025-01-29 18:35:07 -0300

    feat: create sale webapi

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleProfile.cs
@@ -0,0 +1,22 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale;
+
+/// <summary>
+/// Profile for mapping CreateSale feature requests to commands and results to responses
+/// </summary>
+public class CreateSaleProfile : Profile
+{
+    /// <summary>
+    /// Initializes the mappings for CreateSale feature
+    /// </summary>
+    public CreateSaleProfile()
+    {
+        CreateMap<CreateSaleRequest, CreateSaleCommand>();
+        CreateMap<CreateSaleItemRequest, CreateSaleItemCommand>();
+        CreateMap<CreateSaleResult, CreateSaleResponse>();
+        CreateMap<CreateSaleItemResult, CreateSaleItemResponse>();
+    }
+}
